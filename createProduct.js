e125e97c912332978aa72f022eade7077e096911
Hoang Nguyen <hoang___t@Minhs-MacBook-Pro-2.local>
2021-11-21 08:32:45 +0700

    update products schema for new products

--- createProduct.js
+++ createProduct.js
@@ -1,13 +1,16 @@
 const faker = require("faker");
 const Product = require("./models/Product");
-const numberOfProduct = 100;
+const numberOfProduct = 1;
 
 const createProduct = async () => {
   console.log("Creating some products");
   for (let index = 0; index < numberOfProduct; index++) {
     const singleProduct = {
-      name: faker.commerce.product(),
+      name: faker.commerce.productName(),
+      image: faker.image.food(),
       price: parseInt(faker.commerce.price()),
+      description: faker.commerce.productDescription(),
+      category: faker.commerce.department(),
       stock: Math.ceil(Math.random() * 100),
     };
 
--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const productSchema = Schema(
   {
     name: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type:String, required: true},
     price: { type: Number, required: true },
+    category: { type: String, required: true },
     stock: { type: Number, required: true },
     ratings: [
       {
